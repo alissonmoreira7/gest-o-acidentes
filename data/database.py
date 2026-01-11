6e0aa455837ab8ef4afb9bcef41270239b33ba43
alissonmoreira7 <alonmoreira.dev@gmail.com>
2026-01-11 12:51:54 -0300

    Alteração na função de carregar dados - Mundança de tipos das colunas

--- data/database.py
+++ data/database.py
@@ -9,7 +9,9 @@ def init_db():
     sql = '''
     CREATE TABLE IF NOT EXISTS incidentes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+        data_evento TEXT NOT NULL,                      
+        hora_evento TEXT NOT NULL,
         setor TEXT NOT NULL,
         gravidade INTEGER NOT NULL,
         usuario TEXT NOT NULL,
@@ -22,18 +24,18 @@ def init_db():
     con.commit()
     con.close()
 
-def salvar_dados(setor, descricao, gravidade, usuario):
+def salvar_dados(data_evento, hora_evento, setor, gravidade, usuario, descricao):
     con = sqlite3.connect('data/incidentes.db')
     cursor = con.cursor()
 
     sql = '''
-    INSERT INTO incidentes (setor, gravidade, usuario, descricao)
-    VALUES(?, ?, ?, ?)
+    INSERT INTO incidentes (data_evento, hora_evento, setor, gravidade, usuario, descricao)
+    VALUES(?, ?, ?, ?, ?, ?)
     '''
 
-    dados = (setor, gravidade, usuario, descricao)
+    dados = (str(data_evento), str(hora_evento), setor, int(gravidade), usuario, descricao)
+    
     cursor.execute(sql, dados)
-
     con.commit()
     con.close()
 
@@ -43,7 +45,7 @@ def excluir_registro(id_registro):
 
     sql = '''DELETE FROM incidentes WHERE id = ?'''
 
-    cursor.execute(sql, (id_registro))
+    cursor.execute(sql, (id_registro,))
 
     con.commit()
     con.close()
@@ -52,4 +54,12 @@ def carregar_dados():
     con = sqlite3.connect('data/incidentes.db')
     df = pd.read_sql_query("SELECT * FROM incidentes", con)
     con.close()
+
+    if not df.empty:
+        df['data_evento'] = pd.to_datetime(df['data_evento'])
+
+        df['gravidade'] = pd.to_numeric(df['gravidade'], errors='coerce')
+
+        df['hora_num'] = pd.to_datetime(df['hora_evento'], format='%H:%M:%S').dt.hour
+
     return df
